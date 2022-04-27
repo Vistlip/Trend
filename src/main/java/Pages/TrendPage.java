e084351c1efc411a2aa51dfff1749492bc8138a2
Vlad Mikin <asura93@yandex.ru>
2022-04-27 12:22:30 +0300

    27.04.22-2

--- src/main/java/Pages/TrendPage.java
+++ src/main/java/Pages/TrendPage.java
@@ -11,6 +11,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TrendPage {
     public static final String URL = Urls.TrendPageURL;
 
@@ -30,6 +33,16 @@ public class TrendPage {
     private ElementsCollection buttons;
     @FindBy(how = How.XPATH, using = ".//ms-interval")
     private ElementsCollection interval;
+    @FindBy(how = How.XPATH, using = ".//ms-dtpicker")
+    private ElementsCollection dataEnd;
+    @FindBy(how = How.XPATH, using = ".//button")
+    private ElementsCollection buttonsPopup;
+    @FindBy(how = How.CLASS_NAME, using = "btn-open")
+    private SelenideElement openInfo;
+    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div[2]/ul/li/div/div[1]")
+    private SelenideElement openTitle;
+    @FindBy(how = How.CLASS_NAME, using = "body")
+    private SelenideElement messageWarning;
 
 
     //Селекоторы кнопок
@@ -42,8 +55,13 @@ public class TrendPage {
         return page(TrendPage.class);
     }
     public TrendPage clickSelectionTreeButton() {
-        $(ByShadow.cssSelector(".button", "#\\36 67654")).click();
-        return page(TrendPage.class);
+        this.buttons.get(2).click();
+        return this;
+    }
+
+    public String checkSelectionTreeButton() {
+        ElementsCollection div = $$(shadowCss("div", "#\\36 2076", "#toolbar"));
+        return div.get(2).getAttribute("style");
     }
 
     public TrendPage clickButtonBackgroundColorTrend() {
@@ -62,25 +80,6 @@ public class TrendPage {
         return this;
     }
 
-    public TrendPage clickUpdatePeriod() {
-        $(ByShadow.cssSelector("input", "#\\36 2338")).click();
-        return this;
-    }
-
-    public TrendPage inputUpdatePeriod() {
-        $(ByShadow.cssSelector("input", "#\\36 2338")).setValue("1000");
-        return this;
-    }
-
-    public TrendPage clickMaxCountPen() {
-        $(ByShadow.cssSelector("input", "#\\36 2338")).click();
-        return this;
-    }
-
-    public TrendPage inputMaxCountPen() {
-        $(ByShadow.cssSelector("input", "#\\36 2338")).setValue("3");
-        return this;
-    }
 
     public TrendPage clickInterval() {
         $(ByShadow.cssSelector("input", "#\\33 28002")).click();
@@ -98,10 +97,124 @@ public class TrendPage {
         return div.get(1).getAttribute("tooltip");
     }
 
+
     public String checkBackgroundColorTrend() {
         ElementsCollection div = $$(shadowCss("path", "#\\36 2076"));
         return div.get(0).getAttribute("fill");
     }
+
+    public TrendPage clickDataEnd() {
+        this.dataEnd.get(0).click();
+        return this;
+    }
+
+    public TrendPage deleteDataEnd() {
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.BACK_SPACE).perform();
+        return this;
+    }
+
+    public TrendPage inputDataEnd() {
+        Date date = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy HH.mm");
+        String str = formatForDateNow.format(date);
+        System.out.println(str);
+        actions().moveToElement(dataEnd.get(0)).sendKeys(str).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.ENTER).perform();
+        actions().moveToElement(dataEnd.get(0)).sendKeys(Keys.ENTER).perform();
+        return this;
+    }
+
+    public String checkBeginTime() {
+        ElementsCollection text = $$(shadowCss("text", "#\\36 2076"));
+        return text.get(6).getText();
+    }
+
+    public String checkEndTime() {
+        ElementsCollection text = $$(shadowCss("text", "#\\36 2076"));
+        return text.get(11).getText();
+    }
+
+    public String checkTrendToolbar() {
+        ElementsCollection div = $$(shadowCss("ms-toolbar", "#\\36 2076"));
+        return div.get(0).getAttribute("style");
+    }
+
+    public TrendPage clickTitle() {
+        $(ByShadow.cssSelector("input", "#\\36 2386")).click();
+        return this;
+    }
+
+    public TrendPage inputTitle() {
+        $(ByShadow.cssSelector("input", "#\\36 2386")).setValue("Title");
+        return this;
+    }
+
+    public String checkTitle() {
+        ElementsCollection text = $$(shadowCss("text", "#\\36 2076"));
+        int num = text.size()-2;
+        return text.get(num).getText();
+    }
+
+    public TrendPage clickTrendTreePen() {
+        ElementsCollection div = $$(shadowCss("div", "#\\36 2076", "#toolbar"));
+        div.get(2).click();
+        return this;
+    }
+
+    public TrendPage clickCheckboxRandom() {
+        ElementsCollection input = $$(shadowCss("input", "ms-tree"));
+        input.get(3).click();
+        return this;
+    }
+
+    public TrendPage clickCheckboxNotArchivePen() {
+        ElementsCollection input = $$(shadowCss("input", "ms-tree"));
+        input.get(4).click();
+        return this;
+    }
+
+    public TrendPage clickOkTreeTrendPen() {
+        this.buttonsPopup.get(0).click();
+        return this;
+    }
+
+    public TrendPage clickMaxPenInput() {
+        $(shadowCss("input", "#\\33 31094")).click();
+        return this;
+    }
+
+    public TrendPage inputMaxPen() {
+        $(shadowCss("input", "#\\33 31094")).setValue("2");
+        return this;
+    }
+
+    public TrendPage clickInfo() {
+        this.openInfo.click();
+        return this;
+    }
+
+    public TrendPage clickWarningTitle() {
+        this.openTitle.click();
+        return this;
+    }
+
+    public String checkMessageWarning() {
+        return messageWarning.getText();
+    }
+
+
 
     public TrendPage windowsClick() {
         this.windows.get(1).click();
--- src/test/java/CategoriesTrendTest.java
+++ src/test/java/CategoriesTrendTest.java
@@ -1,9 +1,12 @@
 import Pages.TrendPage;
 import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static org.junit.Assert.assertEquals;
+
 
 public class CategoriesTrendTest {
     @Test
@@ -36,12 +39,104 @@ public class CategoriesTrendTest {
     @Test
     @DisplayName("Проверка кнопки автопрокрутка")
     public void checkInterval() throws InterruptedException {
-        TrendPage page = open(TrendPage.URL, TrendPage.class)
+        String beginTime= open(TrendPage.URL, TrendPage.class)
+                .clickDataEnd()
+                .deleteDataEnd()
+                .inputDataEnd()
                 .clickInterval()
                 .inputInterval()
-                .windowsClick();
-        System.out.println(page);
- //       assertEquals("rgba(0,1,255,1.0)", page);
+                .clickDataEnd()
+                .checkBeginTime();
+        String endTime = page(TrendPage.class)
+                .checkEndTime();
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        Date dateOne = null;
+        Date dateTwo = null;
+        try {
+            dateOne = formatForDate.parse(beginTime);
+            dateTwo = formatForDate.parse(endTime);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        long difference = dateTwo.getTime() - dateOne.getTime();
+        int min = (int)(difference / (60 * 1000));
+        System.out.println(min);
+        assertEquals(5, min);
         Thread.sleep(5000);
     }
+
+    @Test
+    @DisplayName("Проверка кнопки автопрокрутка")
+    public void checkEndDate() throws InterruptedException {
+        String page = open(TrendPage.URL, TrendPage.class)
+                .clickDataEnd()
+                .deleteDataEnd()
+                .inputDataEnd()
+                .clickInterval()
+                .inputInterval()
+                .clickDataEnd()
+                .checkEndTime();
+        System.out.println(page);
+        Date date = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        String str = formatForDateNow.format(date) + ":00";
+        assertEquals(str, page);
+        Thread.sleep(5000);
+    }
+
+    @Test
+    @DisplayName("Проверка кнопки автопрокрутка")
+    public void checkMinView() throws InterruptedException {
+        String page = open(TrendPage.URL, TrendPage.class)
+                .clickMinViewButton()
+                .checkTrendToolbar();
+        String[] arrActivity = page.split(";");
+        System.out.println(arrActivity[10]);
+        assertEquals(arrActivity[10]," visibility: hidden");
+        Thread.sleep(5000);
+    }
+
+    @Test
+    @DisplayName("Проверка кнопки автопрокрутка")
+    public void checkTitle() throws InterruptedException {
+        String page = open(TrendPage.URL, TrendPage.class)
+                .clickTitle()
+                .inputTitle()
+                .windowsClick()
+                .checkTitle();
+        System.out.println(page);
+        assertEquals(page, "Title");
+        Thread.sleep(5000);
+    }
+
+    @Test
+    @DisplayName("Проверка кнопки автопрокрутка")
+    public void checkSelectionTree() throws InterruptedException {
+        String page = open(TrendPage.URL, TrendPage.class)
+                .clickSelectionTreeButton()
+                .checkSelectionTreeButton();
+        String[] arrActivity = page.split(";");
+        System.out.println(arrActivity[2]);
+        assertEquals(arrActivity[2]," display: none");
+        Thread.sleep(5000);
+    }
+
+    @Test
+    @DisplayName("Проверка кнопки автопрокрутка")
+    public void checkMaxPen() throws InterruptedException {
+        String page = open(TrendPage.URL, TrendPage.class)
+                .clickMaxPenInput()
+                .inputMaxPen()
+                .clickTrendTreePen()
+                .clickCheckboxRandom()
+                .clickCheckboxNotArchivePen()
+                .clickOkTreeTrendPen()
+                .clickInfo()
+                .clickWarningTitle()
+                .checkMessageWarning();
+        System.out.println(page);
+        assertEquals(page,"Не все параметры были добавлены в тренд (1 не добавлено) из-за превышения ограничения в (2)");
+        Thread.sleep(5000);
+    }
+
 }
