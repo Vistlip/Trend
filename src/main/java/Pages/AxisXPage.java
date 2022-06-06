f5e3ecd18b177733ff61de4c8aec2de0863765b3
Vlad Mikin <asura93@yandex.ru>
2022-06-06 17:27:39 +0300

    06.06.2022

--- src/main/java/Pages/AxisXPage.java
+++ src/main/java/Pages/AxisXPage.java
@@ -1,14 +1,22 @@
 package Pages;
 
+import com.beust.ah.A;
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.selector.ByShadow;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+
 import static com.codeborne.selenide.Selectors.shadowCss;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.actions;
 
 public class AxisXPage {
     public static final String URL = Urls.AxisXURL;
@@ -16,7 +24,11 @@ public class AxisXPage {
     private SelenideElement trend;
     @FindBy(how = How.XPATH, using = ".//ms-window")
     private ElementsCollection windows;
+    @FindBy(how = How.XPATH, using = ".//ms-combobox")
+    private ElementsCollection combobox;
     String trendShadowHost = "#\\37 2100";
+    Duration time = Duration.ofSeconds(15);
+    String labelX = "Привет";
 
     public AxisXPage windowsClick() {
         this.windows.get(1).click();
@@ -34,5 +46,75 @@ public class AxisXPage {
         return this;
     }
 
+    public AxisXPage inputHeightAxis() {
+        $(shadowCss("input", "#\\37 2278")).setValue("20");
+        return this;
+    }
+
+    public String checkHeightAxis() {
+        ElementsCollection path = $$(shadowCss("path", trendShadowHost));
+        String[] splitter = path.get(25).getAttribute("d").split(" ");
+        System.out.println(path.get(25).getAttribute("d"));
+        return splitter[5];
+    }
+
+    public AxisXPage clickDivisions() {
+        combobox.get(1).click();
+        return this;
+    }
+
+    public AxisXPage inputDivisions() {
+        actions().moveToElement(combobox.get(1)).sendKeys("6").perform();
+        actions().moveToElement(combobox.get(1)).sendKeys(Keys.ENTER).perform();
+        return this;
+    }
+
+    public AxisXPage checkDivisions() {
+        ElementsCollection path = $$(shadowCss("path", trendShadowHost));
+        path.shouldHave(CollectionCondition.size(39), time);
+        return this;
+    }
+
+    public AxisXPage inputLabelX() {
+        $(shadowCss("input", "#\\37 2320")).setValue(labelX);
+        return this;
+    }
+
+    public AxisXPage checkLabelX() {
+        ElementsCollection path = $$(shadowCss("text", trendShadowHost));
+        path.get(0).shouldHave(Condition.text(labelX));
+        return this;
+    }
+
+    public AxisXPage inputThicknessAxisX() {
+        $(shadowCss("input", "#\\37 2334")).setValue("9");
+        return this;
+    }
+
+    public AxisXPage checkThicknessAxisX() {
+        ElementsCollection path = $$(shadowCss("path", trendShadowHost));
+        path.get(14).shouldHave(Condition.attribute("stroke-width", "9"), time);
+        return this;
+    }
+
+    public AxisXPage clickDataFormat() {
+        combobox.get(0).click();
+        return this;
+    }
+
+    public AxisXPage inputDataFormatDT() {
+        actions().moveToElement(combobox.get(0)).sendKeys("dd.MM.yyyy HH:mm:ss").perform();
+        actions().moveToElement(combobox.get(0)).sendKeys(Keys.ENTER).perform();
+        return this;
+    }
+
+    public Boolean checkDateFormat() {
+        Date date = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        String str = formatForDateNow.format(date);
+        ElementsCollection path = $$(shadowCss("tspan", trendShadowHost));
+        Boolean format = path.get(10).text().contains(str);
+        return format;
+    }
 
 }
--- src/test/java/CategoriesAxisXTest.java
+++ src/test/java/CategoriesAxisXTest.java
@@ -3,14 +3,68 @@ import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CategoriesAxisXTest {
+    int pause = 500;
     @Test
-    @DisplayName("Проверка толщины делений оси Y")
+    @DisplayName("Проверка: Верхнего отступа оси X")
     public void checkThicknessDivisions() {
         AxisXPage page = open(AxisXPage.URL, AxisXPage.class)
                 .inputTopOffset()
                 .windowsClick()
                 .checkTopOffset();
+    }
+
+    @Test
+    @DisplayName("Проверка: высоты делений оси Х")
+    public void checkHeightAxis() throws InterruptedException {
+        String page = open(AxisXPage.URL, AxisXPage.class)
+                .inputHeightAxis()
+                .windowsClick()
+                .checkHeightAxis();
+        System.out.println(page);
+        assertEquals("620", page);
+        Thread.sleep(pause);
+    }
+
+    @Test
+    @DisplayName("Проверка: Количество делений")
+    public void checkDivisions() {
+        AxisXPage page = open(AxisXPage.URL, AxisXPage.class)
+                .clickDivisions()
+                .inputDivisions()
+                .windowsClick()
+                .checkDivisions();
+    }
+
+    @Test
+    @DisplayName("Проверка: Подписи оси X")
+    public void checkLabelX() {
+        AxisXPage page = open(AxisXPage.URL, AxisXPage.class)
+                .inputLabelX()
+                .windowsClick()
+                .checkLabelX();
+    }
+
+    @Test
+    @DisplayName("Проверка: Толщины оси Х")
+    public void checkThicknessAxisX() {
+        AxisXPage page = open(AxisXPage.URL, AxisXPage.class)
+                .inputThicknessAxisX()
+                .windowsClick()
+                .checkThicknessAxisX();
+    }
+
+    @Test
+    @DisplayName("Проверка: Толщины оси Х")
+    public void checkFormat() {
+        Boolean page = open(AxisXPage.URL, AxisXPage.class)
+                .clickDataFormat()
+                .inputDataFormatDT()
+                .windowsClick()
+                .checkDateFormat();
+        assertTrue(page);
     }
 }
